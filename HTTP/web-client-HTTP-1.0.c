9d2d358ccf5a75a253c73751da93d136b903d809
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-14 15:21:37 +0200

    Update web-client-HTTP-1.0.c

--- HTTP/web-client-HTTP-1.0.c
+++ HTTP/web-client-HTTP-1.0.c
@@ -1,13 +1,9 @@
-
 #include <stdio.h>
 #include <sys/socket.h>             // socket
 #include <errno.h>                  // errno
 #include <arpa/inet.h>              // htons
 #include <unistd.h>                 // write
 #include <string.h>                 // strlen
-
-
-
 
 
 
@@ -53,18 +49,62 @@ int main(){
     }
 
 
-    // richiesta al server
-    char * request = "GET /\r\n";
+    /*
+        Facciamo una richiesta al server attraverso l'HTTP 1.0. La grammatica della simple-request è definita nel modo seguente:
+            Method SP Request-URI CRLF
+        → Method:       che può essere "GET", "HEAD" oppure "POST"
+        → SP:           space
+        → Request-URI:  acronimo di Unifor Request Identifier, identifica la risorsa verso cui applicare la richiesta
+        → CRLF:         dato da \r [carriage return] e \n [new line]
+
+        Scegliendo come richiesta "GET / HTTP/1.0\r\n", si ossera che la richiesta non funziona, in particolare è necessario aggiungere un secondo CRLF. Il primo CRLF è alla fine della simple-requesta ma, nella grammatica, è indicato che è necessario un secondo CRLF al fine di dividere l'HEADER della requesta dal suo BODY.
+        Si osserva infine che se mettiamo HTTP/1.1, la richiesta non funziona perche l'HTTP 1.1 necessita di un altro tipo di grammatica
+    */
+    char * request = "GET / HTTP/1.0\r\n\r\n";
     write(s, request, strlen(request));
 
     // leggo la risposta
     const int RESPONSE_SIZE = 2000000;      // dimensione del buffer
     char response[RESPONSE_SIZE];           // buffer per la risposta
     
-    // sleep(2);
     for ( i = 0; t = read(s, response + i, RESPONSE_SIZE - 1 - i); i += t ) {}
 
+    /*
+        Controlliamo ora che la response sia conforme alla grammatica definita nell'RFC 1945:
+            Simple-Response | Full-Response
+        La Simple-Response non è altro che quella di Berners Lee e contiene solamente il Entity-Body:
+            Simple-Response = [ Entity-Body ]
+
+        La Full-Response invece è composta da un header, opzionale, che può essere di tre tipi e dall'effettivo Entity-Body:
+            Full-Response   = Status-Line
+                            *( General-Header
+                            | Response-Header
+                            | Entity-Header )
+                            CRLF
+                            [ Entity-Body ]
+        Prima di tutto osserviamo che la struttura è molto simile alla request. In secondo luogo, ancora più interessante è che l'Entity-Body è opzionale. Questo ha senso perchè in alcune richiest si vuole conoscere solo lo stato, non necessariamente anche l'Entity-Body.
+
+        Andiamo ora a capire meglio come la full request è strutturata.
+        Nella status line, la prima infromazione è la versione in quanto non deve più sottostare al vincolo di Berners Lee. In secondo luogo, imponendo la versione come prima informazione, la response non potrà mai essere quella di Berners Lee (non viene quindi manteuta la retrocompatibilità). Dopo la versione è presente lo Status-Code con una frase che esplicita l'errore. 
+            Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+                Status-Code
+                    • 1xx: Informational -  Not used, but reserved for future use
+                    • 2xx: Success -        The action was successfully received, understood, and accepted.
+                    • 3xx: Redirection -    Further action must be taken in order to complete the request
+                    • 4xx: Client Error -   The request contains bad syntax or cannot be fulfilled (i.e. 404)
+                    • 5xx: Server Error -   The server failed to fulfill an apparently valid request
+            La Reason-Phrase è stata aggiunte al fine di dare un semplice e rapido debugging, vale anche per il protocollo della posta (SMTP). Questo complica comunque le cose perchè sarà necessario un pareser al fine di 
+
+        Osserviamo che dopo aver stampato a schermo la response, la prima riga è proprio la Status-Line che contiene esattamente la HTTP-Version, lo Status-Code e la sua Reason-Phrase:
+            HTTP/1.0 200 OK
+    */
+
     response[i] = 0;
+    print("%s\n\n", response);
+
+    /*
+        Il prossimo passo consiste nel fare il parsing della risposta, tenendo conto che è uno stream e che dobbiamo farlo nel modo più efficiente possibile. È neecessario creare un parser che prima consumi tutto l'header e poi tutto l'Entity-Body.
+    */
 
 
     return 0;
