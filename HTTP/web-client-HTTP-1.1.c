2200685996357a0b2f02f4f71cb51c24c085aab7
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-16 19:14:39 +0200

    Update web-client-HTTP-1.1.c

--- HTTP/web-client-HTTP-1.1.c
+++ HTTP/web-client-HTTP-1.1.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>              // htons
 #include <unistd.h>                 // write
 #include <string.h>                 // strlen, strcmp
+#include <stdlib.h>                 // atoi
 
 
 
@@ -63,8 +64,15 @@ int main(){
     }
 
 
-    // richiesta HTTP 1.0 al server
-    char * request = "GET / HTTP/1.0\r\n\r\n";
+    /*
+        Se si effettua una richiesta HTTP 1.1 con la stessa sintassi della 1.0, "GET / HTTP/1.1\r\n\r\n", notiamo che dopo aver consumato e stampato l'Header a video, il processo rimane in attesa e non termina l'esecuzione. Uno dei peccato di Berners Lee è stato considerare che la risposta finisse con la chiusura della connessione: non a caso il processo sta eseguendo la read bloccante. 
+        In realtà è più conveniente usare la stessa connessione per fare più richieste. Questo perchè effettuare una connessione costa risorse. Inoltre il flusso dello stream parte lento e poi si stabilizza; aprire molte connessioni rende impossibile questa stabilizzazione rendendo ancora più inefficiente la gestione delle risorse.
+
+        Per questo motivo, dall'HTTP 1.1, di default, non si chiude la connessione non si chiude dopo l'Entity-Body. Per fare ciò, è necessario inserire la stringa "Connection: close" alla fine della richiesta, tra un CRLF e l'altro [GET / HTTP/1.1\r\nConnection: close\r\n\r\n].
+        Scegliere di chiudere la connessione è come fare un downgrade, per tutti i motivi già citati. È quindi necessario capire come gestire questo nuovo comportamento del protocollo.
+        La possibilità di avere una connessione aperta ci permette fare più richiesta senza aprire/chiudere una connessione. Si potrebbe quindi implementare un loop di GET o di altre richieste sulla stessa connessione. Questa cosa con le precedenti versioni del protocollo HTTP non si potevano fare.
+    */
+    char * request = "GET / HTTP/1.1\r\n\r\n";
     write(s, request, strlen(request));
 
 
@@ -72,7 +80,7 @@ int main(){
     /* Lettura della response - header */
 
     // inizializzo il primo puntatore di h[0].n al primo carattere del buffer dell'header (hbuf). Essendo la prima riga, questa è la status line
-    statusline = h[0].n = &hbuf;
+    statusline = h[0].n = hbuf;
     j = 0;
 
     // leggo un carattere alla volta dell'header
@@ -105,18 +113,40 @@ int main(){
 
 
 
-    /* Lettura della response - Body*/
+    /*
+        È proprio durante questo ciclo che si blocca la read in quanto l'Entity-Body è stato letto ed inserito buffer response[] ma dato che la read() non sta ritornando 0 - indice del fatto che la connessone è stata chiusa - si è fermi nel ciclo.
+            for ( i = 0; t = read(s, response + i, RESPONSE_SIZE - 1 - i); i += t ) {}
+        Quando la connessione dopo un po' di tempo di inattività scade, la read() ritorna 0, il ciclo termina e il body viene printato. È quindi necessario modificare il ciclo for per gestire questa situazione.
+
+        La soluzione non è del tutto banale. Nel caso in cui la richiesta abbia un errore, per esempio richiediamo una risorsa inesistente (come ppp.html), nell'header sarà presente un campo chiamato "Content-Length" che indica esattamente la lunghezza del file che verrà streamato. In questo caso l'idea è quella di trovare questo campo mentre stampiamo a video l'header, per chiarezza si sceglierà di creare un altro for loop ad-hoc per trovare il suddetto valore.
+        Cosa succede però se la richiesta è ben formata e va a buon fine? Si sserva che poco prima dell'inizio documento HTML è presente un numero esadecimale (i.e. 493c). Si osserva inoltre che l'ultimo campo dell'Header ha valore "Transfer-Encoding —————>  chunked" che, come vedremo, suggerisce il tipo della codifica di trasferimento. Il valore esadecimale indica ESATTAMENTE il numero di byte contenuti all'interno dello stream che andremo a leggere.
+    */
+
+    int content_length;
+
+    // estraggo la Content-Length dal h[i]
+    for(i = 0; i < j; i++){
+        /*
+            Si utilizza la funzione strcmp() che ritorna 0 se le due stringhe sono uguali.
+            Si utilizza poi la funzione atoi() per immagazzinare il valore trovato convertendolo in un intero
+        */
+        if( !strcmp( h[i].n , "Content-Length" ))
+            content_length = atoi(h[i].v);
+    }
+    printf("content-length: %d\n", content_length);
 
     // preparo a leggere l'Entity-Body
-    const int RESPONSE_SIZE = 2000000;      // dimensione del buffer
-    char response[RESPONSE_SIZE];           // buffer per la consumare il Body
+    char response[2000000];           // buffer per la consumare il Body
     
-    // leggo l'Entity-Body
-    for ( i = 0; t = read(s, response + i, RESPONSE_SIZE - 1 - i); i += t ) {}
+
+    /*
+        Al fine di supportare questo protocollo, si modifica il limite della read() a content_length che è esattamente la lunghezza in byte - ovvero il numero di caratteri - del file streammato.
+    */
+    for ( i = 0; t = read(s, response + i, content_length - i); i += t ) {}
 
     response[i] = 0;    // inserisco il terminatore alla fine del body
 
-    print("%s\n\n", response);
+    printf("%s\n\n", response);
 
 
 
