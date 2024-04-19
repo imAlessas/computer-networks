351ae0d419b0a624f916cb5c9b4a6822fd8b5101
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-20 00:02:41 +0200

    started hw-1 task1

--- multimedia/hw-1/script/lossless_coding.m
+++ multimedia/hw-1/script/lossless_coding.m
@@ -1,2 +1,18 @@
-clc, clearvars, clear, format compact
+clc, clearvars, clear, close, format compact
 
+
+% Task 1 - Load the image and estime its entropy
+
+img_file_name = 'rocky-beach-greys.jpg';
+img_folder = '../imgs/';
+
+img = imread("" + img_folder + img_file_name);
+
+% creates figure and settings
+f = figure(1);
+f.Name = 'Image';
+f.NumberTitle = 'off';
+f.Position = [450, 100, 700, 600];
+
+% displays figure
+imagesc(img), colormap(gray), axis image, axis off
