ddab00f2116580866ec7baf9345ab4ce659176bc
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-22 19:57:44 +0200

    multimedia/hw-1 task-4

--- multimedia/hw-1/script/lossless_coding.py
+++ multimedia/hw-1/script/lossless_coding.py
@@ -24,19 +24,17 @@ img = mpimg.imread(path_to_img +  img_file_name + img_extension)
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # creates a figure with two subplots
-# fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
 
 # displays the colored image in the first subplot
-# axs[0].imshow(img, cmap='gray')
-# axs[0].set_title('Colored image')
-# axs[0].axis('off')
+axs[0].imshow(img, cmap='gray')
+axs[0].set_title('Colored image')
+axs[0].axis('off')
 
 # displays the grayscale image in the second subplot
-# axs[1].imshow(gray_img, cmap='gray')
-# axs[1].set_title('Grayscale image')
-# axs[1].axis('off')
-
-# plt.show()
+axs[1].imshow(gray_img, cmap='gray')
+axs[1].set_title('Grayscale image')
+axs[1].axis('off')
 
 
 
@@ -49,10 +47,10 @@ rasterScan = np.transpose(gray_img).flatten()
 occurrencies = np.histogram(rasterScan, bins=range(256))[0]
 
 # calculate the relative frequencies
-p = occurrencies / np.sum(occurrencies)
+rel_freq = occurrencies / np.sum(occurrencies)
 
 # remove zero-values of probability
-p = p[p > 0]
+p = rel_freq[rel_freq > 0]
 
 # compute and display the entropy
 HX = - np.sum(p * np.log2(p))
@@ -85,6 +83,33 @@ print(f"\nThe bitrate of {img_file_name}.zip is {zip_bitrate:.3f} bpp\n")
 
 
 
+#######    Task 4    #######
+
+# calculate prediction error
+pred_err = rasterScan[0] - 128
+pred_err = np.append(pred_err, np.diff(rasterScan))
+
+# plot error graph
+plt.figure()
+plt.imshow(np.transpose(np.reshape(np.abs(pred_err), (width, height))), cmap = 'seismic')
+plt.axis('image')
+plt.axis('off')
+plt.colorbar()
+plt.title('Prediction Error Magnitude')
+plt.show()
+
+
+# count the occurrences of each prediction error value
+occ, _ = np.histogram(pred_err, bins = range(-255, 256))
+
+# calculate the relative frequencies and remove any probability == 0
+freqRel = occ / np.sum(occ)
+p = freqRel[freqRel > 0]
+
+# calculate the entropy
+HY = -np.sum(p * np.log2(1/p))
+print(f"The entropy of the prediction error of {img_file_name} is {HY:.3f} bpp")
+print(f"The compression ratio of {img_file_name} is {8/HX:.4f}\n")
 
 
 
