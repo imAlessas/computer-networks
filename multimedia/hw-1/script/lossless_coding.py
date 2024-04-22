d76ed140dad84a844e45e6100ebd1470da123dc1
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-22 19:09:20 +0200

    multimedia/hw-1 task-2

--- multimedia/hw-1/script/lossless_coding.py
+++ multimedia/hw-1/script/lossless_coding.py
@@ -10,13 +10,15 @@ import cv2
 ## Display the images
 
 # Prepare to load the image
-img_file_name = "spiderman.jpg"
+img_file_name = "spiderman"
+img_extension = ".jpg"
 current_dir = os.getcwd()
 
-img_path = os.path.join(current_dir, "multimedia", "hw-1", "script", "imgs", img_file_name) # construct the full path to the image file
+# path to reach the img
+path_to_img = os.path.join(current_dir, "multimedia", "hw-1", "script", "imgs") + "/"
 
 # loads the colored image
-img = mpimg.imread(img_path) 
+img = mpimg.imread(path_to_img +  img_file_name + img_extension) 
 
 # extracts the luminance
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -54,8 +56,24 @@ p = p[p > 0]
 
 # compute and display the entropy
 HX = - np.sum(p * np.log2(p))
-print(f"\nThe entropy of {img_file_name} is {HX:.3f} bpp\n")
+print(f"\nThe entropy of {img_file_name}{img_extension} is {HX:.3f} bpp")
+print(f"The compression ratio of {img_file_name}{img_extension} is {8/HX:.4f}\n")
+
+
 
 
 
 #######    Task 2    #######
+
+# zip the image
+cmd = f"zip {path_to_img}{img_file_name}.zip {path_to_img}{img_file_name}.jpg"
+os.system(cmd)
+
+# get the zip bytes
+img_stats = os.stat(f"{path_to_img}{img_file_name}.zip")
+zip_bytes = img_stats.st_size
+
+# get the birate
+zip_bitrate = zip_bytes * 8 / (512 * 512) 
+
+print(f"\nThe bitrate of {img_file_name}.zip is {zip_bitrate:.3f} bpp\n")
