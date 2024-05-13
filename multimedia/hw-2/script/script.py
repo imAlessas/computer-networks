14fd8deab2d4630d1b95eaba10c45bb4a45b56b6
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-05-13 20:14:26 +0200

    Update script.py
    - utilized ThreadPoolExecutor to drastically improve code execution time
    - utilized subprocess instead of os.system()
    - added time() to keep track of the execution time
    - calculated min(), max(), avg() and atddev() for every length
    - utilized sklearn to obtain alpha with linear regression
    - plotted data

--- multimedia/hw-2/script/script.py
+++ multimedia/hw-2/script/script.py
@@ -1,24 +1,60 @@
 import os
-import pprint
+import time
+import math
+import subprocess
 import numpy as np
-import matplotlib.pyplot as plt
+import concurrent.futures
+from sklearn.linear_model import LinearRegression
 
 
 from utilities import *
 
 
 
+def ping_server(server, instances, length):
+
+    print(f" Payload length:  {length}", end="\r")
+
+    cmd = f"ping {server} -n {instances} -l {length}"
+    
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    millisecs_vector = []
+    lines = result.stdout.split("\n")
+    
+    for line in lines:
+        if "durata=" in line:
+            duration = line.split("durata=")[1].split("ms")[0]
+            millisecs_vector.append(int(duration))
+    
+    return length, millisecs_vector
 
 
-def get_links_from_tracert(filename : str) -> int:
+
+
+
+
+def get_links_from_tracert(server: str, filename: str) -> int:
 
     # get number of links from tracert
-    cmd = f"tracert {server} > {filename}"
-    os.system(cmd)
+    cmd = f"tracert {server}"
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    with open(f"{filename}", "r") as file:
-        last_router_line = file.readlines()[-3]
-        return int(last_router_line.split(" ")[1])
+    with open(f"{filename}", "a") as file:
+        file.write(result.stdout)
+
+    last_link_line = result.stdout.split("\n")[-4]
+
+    return int(last_link_line.split(" ")[1])
+
+
+
+
+
+
+
+
+
 
 
 
@@ -26,11 +62,13 @@ def get_links_from_tracert(filename : str) -> int:
 
 
 if __name__ == "__main__":
+    START = time.time()
 
     # setup for the script
-    SELECTED_SERVER_CITY = "Lyon"
+    SELECTED_SERVER_CITY = "Los Angeles"
     INSTANCES = 100
-    PAYLOAD_LENGHTS = range(10, 1470, 75)
+    STEP = 100
+    PAYLOAD_LENGTHS = range(10, 1471, STEP)
 
     # Definitions of constants
     LOGS_PATH = os.path.join("multimedia", "hw-2", "script", "logs") + "\\"
@@ -51,6 +89,11 @@ if __name__ == "__main__":
     city = server.split(".")[0]
 
 
+
+    print(f"\nServer:         \033[1m\033[34m{server}\033[0m\n\n")
+
+
+
     # delete files in the directory
     delete_files_in_directory(LOGS_PATH, city)
 
@@ -58,25 +101,34 @@ if __name__ == "__main__":
 
     # count number of links
     print_task(1, number_color="red")
-
-    filename = f"{LOGS_PATH}{city}-links.txt"
-    tracert_links = get_links_from_tracert(filename)
-    print(f"Number of links found with \033[1mtracert\033[0m: {tracert_links}")
-
-
     
+    # using tracetr
+    start = time.time()
+
+    filename = f"{LOGS_PATH}{city}-links-tracert.txt"
+    tracert_links = get_links_from_tracert(server, filename)
+   
+    print(f" Number of links found with \033[1mtracert\033[0m: {tracert_links}")
+    print(f"                { round(time.time() - start, 2) } sec")
+
+
+    # using multiple ping
+    start = time.time()
+    filename = f"{LOGS_PATH}{city}-links-ping.txt"
+
     ping_links = 0
     for ttl in range(20, 0, -1):
 
-        cmd = f"ping {server} -i {ttl} > {filename}"
-        os.system(cmd)
+        cmd = f"ping {server} -i {ttl}"
+        result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-        with open(f"{filename}", "r") as file:
-           if "TTL scaduto durante il passaggio" in file.read():
-               ping_links = ttl + 1
-               break
+        if "TTL scaduto durante il passaggio" in result.stdout:
+            ping_links = ttl + 1
+            break
            
-    print(f"Number of links found with muliple \033[1mping\033[0m: {ping_links}")
+    print(f" Number of links found with muliple \033[1mping\033[0m: {ping_links}")
+    print(f"                { round(time.time() - start, 2) } sec")
+
 
 
 
@@ -85,54 +137,115 @@ if __name__ == "__main__":
     
     # minimum, maximum, average RTT
     print_task(2, number_color="red")
+    start = time.time()
 
     stats = {}
 
     filename = f"{LOGS_PATH}{city}-RTT.txt"
 
-    for lenght in PAYLOAD_LENGHTS:
-        print(f"  Payload length :     {lenght}")
-
-        millisecs_vector = []
-
-        cmd = f"ping {server} -n {INSTANCES} > {filename}"
-        os.system(cmd)
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
         
-        with open(f"{filename}", "r") as file:
-            lines = file.readlines()
+        futures = []
+        for length in PAYLOAD_LENGTHS:
+            future = executor.submit(ping_server, server, INSTANCES, length)
+            futures.append(future)
+            time.sleep(.75)
+        
+        for future in concurrent.futures.as_completed(futures):
+            length, millisecs_vector = future.result()
+            stats[length] = millisecs_vector
+    
 
-            for line in lines:
-                if "durata=" in line:
-                    duration = line.split("durata=")[1].split("ms")[0]
-                    millisecs_vector.append(int(duration))
+    save_dictionary(stats, filename)
 
-        stats[lenght] = millisecs_vector
-
-
-    pprint.pprint(stats)
+    print(f"\n Processed {len(PAYLOAD_LENGTHS) * INSTANCES} pings")
+    print(f"                { round(time.time() - start, 2) } sec")
 
 
 
-
-
-    print_task(3, number_color="red")
-
-    # Extract payload lengths and durations
+    # extract payload lengths and durations
     payload_lengths = list(stats.keys())
     durations = [item for sublist in stats.values() for item in sublist]
 
-    # Create random edge colors for each point
-    edge_colors = np.random.rand(len(payload_lengths) * INSTANCES, 3)
+    # plot durations
+    plot_data(payload_lengths * INSTANCES,
+              durations,
+              edge="random",
+              x_label="L (pkt size) - bytes",
+              y_label="RTT(k) - ms")
 
-    # Create the scatter plot with random edge colors and no fill color
-    plt.scatter(payload_lengths * INSTANCES, durations, edgecolors=edge_colors, facecolors='none')
 
-    # Set the labels and title
-    plt.xlabel('L (pkt size) - bits')
-    plt.ylabel('RTT(k) - ms')
 
-    # Show grid
-    plt.grid(True)
 
-    # Display the plot
+    start = time.time()
+
+    # compute the min, max and avg of stats
+    v_max = {}
+    v_min = {}
+    v_avg = {}
+    v_std = {}
+
+    for key, value in stats.items():
+        v_max[key] = max(value)
+        v_min[key] = min(value)
+        v_avg[key] = sum(value) / len(value)
+        v_std[key] = math.sqrt(sum((x - v_avg[key]) ** 2 for x in value) / len(value))
+
+    print(" Computed Min, Max, Avg and StdDev")
+    print(f"                { round(time.time() - start, 2) } sec")
+
+
+    plot_data(payload_lengths,
+        list(v_max.values()),
+        edge="red",
+        x_label="L (pkt size) - bytes",
+        y_label="RTT(k) max - ms")
+    
+    plot_data(payload_lengths,
+        list(v_min.values()),
+        edge="yellow",
+        x_label="L (pkt size) - bytes",
+        y_label="RTT(k) min - ms")
+
+    plot_data(payload_lengths,
+        list(v_std.values()),
+        edge="cyan",
+        x_label="L (pkt size) - bytes",
+        y_label="RTT(k) std - ms")
+
+    plot_data(payload_lengths,
+        list(v_avg.values()),
+        edge="red",
+        x_label="L (pkt size) - bytes",
+        y_label="RTT(k) avg - ms")
+
+
+
+
+
+
+    # alpha-coefficient and throughput
+    print_task(3, number_color="red")
+    start = time.time()
+
+    min_values = np.array([[v] for v in list(v_min.values())])
+    reg = LinearRegression().fit(min_values , np.array(PAYLOAD_LENGTHS))
+
+    alpha = reg.coef_
+    throughput_identical_link = 2 * tracert_links / alpha
+    throughput_bottleneck = 2 / alpha
+    
+    print(f"alpha = {alpha}")
+    print(f"Throoughput with identical links = {throughput_identical_link}")
+    print(f"Throughpunt in a bottleneck scenario = {throughput_bottleneck}")
+    print(f"                { round(time.time() - start, 2) } sec")
+    
+    
+
+
+
+    
     plt.show()
+
+    print(f"\n\n\nTotal execution time:     { round(time.time() - START, 2) } sec")
